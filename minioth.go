ac1434c64212b5771c8cf5d006b6694daa68a291
Kyriakos Chalvatzis <kyriakos.chalvatzis@gmail.com>
2024-12-04 04:32:57 +0200

    worked on the api, issuing tokens atp

--- minioth.go
+++ minioth.go
@@ -12,10 +12,15 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// I want to implement a simplistic but handy user/group state system.
-// Let's copy the UNIX model
-//
-// /etc/passwd, /etc/shadow, /etc/group
+/* I want to implement a simplistic but handy user/group state system.
+* Let's copy the UNIX model
+*
+* /etc/passwd, /etc/shadow, /etc/group
+*
+* TODO: This program should provide atomicity perhaps
+*
+* WARNING: not currently threadsafe...
+ */
 
 const (
 	MINIOTH_PASSWD string = "data/mpasswd"
@@ -24,8 +29,16 @@ const (
 
 	HASH_COST  int    = 16
 	MINIOTH_DB string = "data/minioth.db"
+
+	PLACEHOLDER_PASS string = "3ncrypr3d"
+	DEL              string = ":"
+	// username:password ref:uuid:guid:info:home:shell
+	ENTRY_MPASSWD_FORMAT string = "%s" + DEL + "%s" + DEL + "%v" + DEL + "%v" + DEL + "%s" + DEL + "%s" + DEL + "%s\n"
+	ENTRY_MSHADOW_FORMAT string = "%s" + DEL + "%s" + DEL + "%s" + DEL + "%s" + DEL + "%s" + DEL + "%s" + DEL + "%s" + DEL + "%s" + DEL + "%v\n"
+	ENTRY_MGROUP_FORMAT  string = "%s" + DEL + "%s" + DEL + "%v\n"
 )
 
+/* idk if this is useful yet.. */
 type userspace interface {
 	useradd() error
 	userdel() error
@@ -49,25 +62,30 @@ type User struct {
 }
 type Password struct {
 	Hashpass           string `json:"hashpass"`
-	LastPasswordChange string `json:"lpasschange"`
-	MinPasswordAge     string `json:"minpassage"`
-	MaxPasswordAge     string `json:"maxpassage"`
-	WarningPeriod      string `json:"warningperiod"`
-	InactivityPeriod   string `json:"inactivityperiod"`
-	ExpirationDate     string `json:"expirationdate"`
-	Length             int    `json:"passlength"`
+	LastPasswordChange string `json:"lastPasswordChange"`
+	MinPasswordAge     string `json:"minimumPasswordAge"`
+	MaxPasswordAge     string `json:"maximumPasswordAge"`
+	WarningPeriod      string `json:"warningPeriod"`
+	InactivityPeriod   string `json:"inactivityPeriod"`
+	ExpirationDate     string `json:"expirationDate"`
+	Length             int    `json:"passwordLength"`
 }
 type Group struct {
-	Name     string `json:"groupname" form:"groupname"`
-	Password Password
-	Users    []User
-	Gid      int64 `json:"gid" form:"gid"`
+	Name  string `json:"groupname" form:"groupname"`
+	Users []User
+	Gid   int64 `json:"gid" form:"gid"`
 }
 
-func Hash(password []byte) ([]byte, error) {
+func (m *Minioth) sync() error {
+	return nil
+}
+
+/* use bcrypt blowfish algo (and std lib) to hash a byte array */
+func hash(password []byte) ([]byte, error) {
 	return bcrypt.GenerateFromPassword(password, HASH_COST)
 }
 
+/* check if a passowrd is correct */
 func verifyPass(hashedPass, password []byte) bool {
 	if err := bcrypt.CompareHashAndPassword(hashedPass, password); err == nil {
 		return true
@@ -78,13 +96,14 @@ func verifyPass(hashedPass, password []byte) bool {
 type Minioth struct {
 	dbPath string
 
-	root       User
-	usercount  int
-	groupcount int
+	root       User /* perhaps we dont need to hold a ref to this, but each Minioth should have a root user. Its handled on init*/
+	usercount  int  /* just stats.. */
+	groupcount int  /* just stats.. */
 
-	useDB bool
+	useDB bool /* for intrgrating with a db. NOTE:sqlite (for now)*/
 }
 
+/* Use this function to create an instance of minioth. */
 func NewMinioth(rootname string, useDb bool, dbPath string) Minioth {
 	log.Print("Creating new minioth")
 	newM := Minioth{
@@ -108,31 +127,75 @@ func NewMinioth(rootname string, useDb bool, dbPath string) Minioth {
 	return newM
 }
 
+/* delete the 3 state files */
+func (m *Minioth) purge() {
+	log.Print("Purging everything...")
+
+	_, err := os.Stat("data")
+	if err != nil {
+		log.Printf("error stating data dir: %v", err)
+		err = os.Mkdir("data", 0o700)
+		if err != nil {
+			panic("failed to make new directory.")
+		}
+	}
+
+	err = os.Remove(MINIOTH_PASSWD)
+	if err != nil {
+		log.Print(err)
+	}
+	err = os.Remove(MINIOTH_GROUP)
+	if err != nil {
+		log.Print(err)
+	}
+	err = os.Remove(MINIOTH_SHADOW)
+	if err != nil {
+		log.Print(err)
+	}
+	err = os.Remove(MINIOTH_DB)
+	if err != nil {
+		log.Print(err)
+	}
+
+	// TODO: delete more stuff.>!!!!
+}
+
+/*  */
+/* Pretty important!*/
+/* initialization routines. check if data directory is there, check if root user exists...*/
 func (m *Minioth) init() {
 	log.Print("Initializing minioth")
 
 	_, err := os.Stat("data")
 	if err != nil {
 		log.Printf("error stating data dir: %v", err)
-		err = os.Mkdir("data", 0700)
+		err = os.Mkdir("data", 0o700)
 		if err != nil {
 			panic("failed to make new directory.")
 		}
 	}
-	// Reset file
-	err = os.Remove(MINIOTH_PASSWD)
-	if err != nil {
-		log.Print(err)
+
+	// TODO: should check for existing files integrity...
+	log.Print("Checking if data file exists...")
+
+	// Should check if root exists...
+	if err := verifyFilePrefix(MINIOTH_PASSWD, "root"); err != nil {
+		// Add root user
+		m.Useradd(User{
+			Name: "root",
+			Password: Password{
+				Hashpass: "root",
+			},
+			Uid:    0,
+			Pgroup: 0,
+			Info:   "HEADMASTER",
+			Home:   "/",
+			Shell:  "/bin/gshell",
+		})
 	}
-
-	// Add root user
-	m.Useradd("root", "root", "Root of the system", "/", "/bin/gshell")
 }
 
-func (m *Minioth) sync() error {
-	return nil
-}
-
+/* this method is supposed to return eveyrhing from the given file */
 func (m *Minioth) Select(id string) []string {
 	log.Printf("Selecting all %q", id)
 	switch id {
@@ -163,8 +226,18 @@ func (m *Minioth) Select(id string) []string {
 	}
 }
 
-func (m *Minioth) Useraddu(user User) error {
+/* This function doesn't check if user already exists. */
+func (m *Minioth) Useradd(user User) error {
 	log.Printf("Adding user %q ...", user.Name)
+
+	// Generate password early, return early if failed...
+	hashPass, err := hash([]byte(user.Password.Hashpass))
+	if err != nil {
+		log.Printf("Failed to hash the pass... :%v", err)
+		return err
+	}
+
+	// Open/Create files first to handle all file errors at once.
 	file, err := os.OpenFile(MINIOTH_PASSWD, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0o600)
 	if err != nil {
 		log.Printf("error opening file: %v", err)
@@ -172,49 +245,91 @@ func (m *Minioth) Useraddu(user User) error {
 	}
 	defer file.Close()
 
-	if exists(user.Name, file) {
-		// TODO: handle error cases
+	pfile, err := os.OpenFile(MINIOTH_SHADOW, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0o600)
+	if err != nil {
+		log.Printf("error opening file: %v", err)
 		return err
 	}
+	defer pfile.Close()
 
+	gfile, err := os.OpenFile(MINIOTH_GROUP, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0o600)
+	if err != nil {
+		log.Printf("error opening file: %v", err)
+		return err
+	}
+	defer gfile.Close()
+
+	// passwd file
 	// get uuid
 	uuid := nextUid()
+	fmt.Fprintf(file, ENTRY_MPASSWD_FORMAT, user.Name, PLACEHOLDER_PASS, uuid, uuid, user.Info, user.Home, user.Shell)
 
-	file.WriteString(strings.Join([]string{user.Name, "3ncrypr3d", uuid, uuid, user.Info, user.Home, user.Shell}, ":") + "\n")
+	// file.WriteString(strings.Join([]string{user.Name, "3ncrypr3d", uuid, uuid, user.Info, user.Home, user.Shell}, DEL) + "\n")
+
+	// shadow file
+	fmt.Fprintf(pfile, ENTRY_MSHADOW_FORMAT, user.Name, hashPass, user.Password.LastPasswordChange, user.Password.MinPasswordAge, user.Password.MaxPasswordAge, user.Password.WarningPeriod, user.Password.InactivityPeriod, user.Password.ExpirationDate, user.Password.Length)
 
 	log.Print("Useradd successful.")
+	m.usercount++
 	return nil
 }
 
-func (m *Minioth) Useradd(username, password, info, home, shell string) error {
-	log.Printf("Adding user %q ...", username)
-	file, err := os.OpenFile(MINIOTH_PASSWD, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0o600)
+/* approval of minioth means, user exists and password is valid */
+func (m *Minioth) approveUser(username, password string) bool {
+	log.Printf("approving user... %q:%q", username, password)
+
+	file, err := os.Open(MINIOTH_PASSWD)
+	if err != nil {
+		log.Printf("failed to open file: %v", err)
+		return false
+	}
+	defer file.Close()
+
+	pfile, err := os.Open(MINIOTH_SHADOW)
+	if err != nil {
+		log.Printf("failed to open file: %v", err)
+		return false
+	}
+	defer pfile.Close()
+
+	log.Print("searching for user entry...")
+	userline, line, err := search(username, file)
+	if err != nil || line == -1 {
+		log.Printf("failed to search for user: %v", err)
+		return false
+	}
+	log.Print(userline)
+
+	log.Print("searching for password entry...")
+	passline, pline, err := search(username, pfile)
+	if err != nil || pline == -1 {
+		log.Printf("failed to search for pass: %v", err)
+		return false
+	}
+
+	// log.Printf("userentry: %s, passwordentry: %s", userline, passline)
+
+	hashpass := strings.SplitN(passline, DEL, 3)[1]
+
+	return verifyPass([]byte(hashpass), []byte(password))
+}
+
+/* check if a user is already here. Error nil if not*/
+func exists(user *User) error {
+	log.Printf("checking if %q exists...", user.Name)
+	file, err := os.Open(MINIOTH_PASSWD)
 	if err != nil {
 		log.Printf("error opening file: %v", err)
 		return err
 	}
 	defer file.Close()
-
-	if exists(username, file) {
-		// TODO: handle error cases
-		return err
-	}
-
-	// get uuid
-	uuid := nextUid()
-
-	file.WriteString(strings.Join([]string{username, "3ncrypr3d", uuid, uuid, info, home, shell}, ":") + "\n")
-
-	log.Print("Useradd successful.")
-	return nil
-}
-
-func exists(username string, file *os.File) bool {
-	if res, line, err := search(username, file); err == nil {
+	res, line, err := search(user.Name, file)
+	if err == nil && line != -1 {
 		log.Printf("user: %q exists at line: %v", strings.SplitN(res, ":", 2)[0], line)
-		return true
+		return errors.New("user already exists")
 	}
-	return false
+
+	return nil
 }
 
 // must have
@@ -243,6 +358,8 @@ func search(username string, file *os.File) (string, int, error) {
 	return "", -1, fmt.Errorf("user not found")
 }
 
+/* Look for all the existing uids and give the succeeding one in order.
+ * It verifies uniqueness*/
 func nextUid() string {
 	f, err := os.Open(MINIOTH_PASSWD)
 	if err != nil {
@@ -286,6 +403,7 @@ func nextUid() string {
 	return strconv.Itoa(iuuid)
 }
 
+/* simply delete a user.. */
 func (m *Minioth) Userdel(username string) error {
 	log.Printf("Deleting user %q ...", username)
 	if username == "" {
@@ -359,6 +477,7 @@ func (m *Minioth) passwd(username, password string) error {
 	return nil
 }
 
+/* check password fields for allowed values...*/
 func (p *Password) validatePassword() error {
 	// Validate Password Length
 	if p.Length < 8 {
@@ -403,6 +522,7 @@ func (p *Password) validatePassword() error {
 	return nil
 }
 
+// Unused
 func checkIfUserExists(u User) error {
 	log.Printf("Checking if name:%s, already exists...", u.Name)
 	var users []string
@@ -428,4 +548,31 @@ func checkIfUserExists(u User) error {
 	}
 
 	return nil
+}
+
+/* just read the first 4 bytes from a file...
+* Used to check if root is entried.*/
+func verifyFilePrefix(filePath, prefix string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 4)
+
+	n, err := file.Read(buffer)
+	if err != nil {
+		return fmt.Errorf("failed to read from file: %w", err)
+	}
+
+	if n < 4 {
+		return fmt.Errorf("file is too short, only read %d bytes", n)
+	}
+
+	if string(buffer) == prefix {
+		return nil
+	}
+
+	return fmt.Errorf("prefix doesn't match %s", prefix)
 }
--- minioth_server.go
+++ minioth_server.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type MService struct {
@@ -25,8 +26,13 @@ const (
 	DEFAULT_audit_log_path string = "data/minioth.log"
 )
 
-type UserClaim struct {
+type RegisterClaim struct {
 	User User `json:"user"`
+}
+
+type LoginClaim struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func offLimits(str string) bool {
@@ -36,7 +42,23 @@ func offLimits(str string) bool {
 	return false
 }
 
-func (u *UserClaim) validateUser() error {
+func (l *LoginClaim) validateClaim() error {
+	if l.Username == "" {
+		return errors.New("username cannot be empty")
+	}
+
+	if !IsAlphanumericPlus(l.Username) {
+		return fmt.Errorf("username %q is invalid: only alphanumeric chararctes[@+] are allowed", l.Username)
+	}
+
+	if l.Password == "" {
+		return errors.New("password cannot be empty")
+	}
+
+	return nil
+}
+
+func (u *RegisterClaim) validateUser() error {
 	if u.User.Name == "" {
 		return errors.New("username cannot be empty")
 	}
@@ -72,6 +94,7 @@ func (u *UserClaim) validateUser() error {
 
 func NewMSerivce(m *Minioth, conf string) MService {
 	cfg := LoadConfig(conf)
+	log.Print(cfg.ToString())
 
 	srv := MService{
 		Minioth: m,
@@ -93,17 +116,16 @@ func (srv *MService) ServeHTTP() {
 		// /token/refresh
 		// /health, /audit/logs, /admin/users, /admin/users
 
-		apiV1.POST("/login", func(c *gin.Context) {
-		})
-
 		apiV1.POST("/register", func(c *gin.Context) {
-			var uclaim UserClaim
-			err := c.Bind(&uclaim)
+			var uclaim RegisterClaim
+			err := c.BindJSON(&uclaim)
 			if err != nil {
 				log.Printf("error binding request body to struct: %v", err)
 				c.Error(err)
 				return
 			}
+
+			log.Printf("%+v", uclaim)
 			// Verify user credentials
 			err = uclaim.validateUser()
 			if err != nil {
@@ -114,7 +136,7 @@ func (srv *MService) ServeHTTP() {
 				return
 			}
 			// Check for uniquness
-			err = checkIfUserExists(uclaim.User)
+			err = exists(&uclaim.User)
 			if err != nil {
 				log.Printf("failed checking for uniqueness...: %v", err)
 				c.JSON(500, gin.H{
@@ -123,11 +145,55 @@ func (srv *MService) ServeHTTP() {
 				return
 			}
 			// Proceed with Registration
+			srv.Minioth.Useradd(uclaim.User)
+
 			c.JSON(200, gin.H{
 				"username": uclaim.User.Name,
 				"password": uclaim.User.Password.Hashpass,
 			})
+		})
 
+		apiV1.POST("/login", func(c *gin.Context) {
+			var lclaim LoginClaim
+			err := c.BindJSON(&lclaim)
+			if err != nil {
+				log.Printf("error binding request body to struct: %v", err)
+				c.Error(err)
+				return
+			}
+
+			log.Printf("login claim: %+v", lclaim)
+			// Verify user credentials
+			err = lclaim.validateClaim()
+			if err != nil {
+				log.Printf("failed to validate: %v", err)
+				c.JSON(400, gin.H{
+					"error": err.Error(),
+				})
+				return
+			}
+			log.Print("claim validated")
+
+			// TODO: approve user
+			approved := srv.Minioth.approveUser(lclaim.Username, lclaim.Password)
+			if !approved {
+				log.Printf("invalid credentials. login failed")
+				c.JSON(400, gin.H{
+					"error": "invalid credentials",
+				})
+				return
+			}
+			log.Print("claim approved")
+
+			// TODO: issue a token
+			//
+			// TODO: idk, propably a cookie... or sth(session)
+
+			token := "t0k3n_1s_r4nd0m"
+			c.JSON(200, gin.H{
+				"username": lclaim.Username,
+				"token":    token,
+			})
 		})
 
 		apiV1.POST("/logout", func(c *gin.Context) {
@@ -151,6 +217,15 @@ func (srv *MService) ServeHTTP() {
 			c.JSON(http.StatusOK, gin.H{
 				"content": minioth.Select("users"),
 			})
+		})
+
+		admin.POST("/useradd", func(c *gin.Context) {
+		})
+
+		admin.DELETE("/userdel", func(c *gin.Context) {
+		})
+
+		admin.PUT("/usermod", func(c *gin.Context) {
 		})
 
 		admin.GET("/groups", func(c *gin.Context) {
@@ -186,4 +261,59 @@ func (srv *MService) ServeHTTP() {
 	}
 
 	log.Println("Server exiting")
+}
+
+var jwtSecretKey = []byte("your_secret_key")
+
+// CustomClaims represents the claims for the JWT token
+type CustomClaims struct {
+	UserID string `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userID, role string) (string, error) {
+	// Set the claims for the token
+	claims := CustomClaims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "minioth",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)), // Token expiration time (24 hours)
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   userID,
+		},
+	}
+
+	// Create the token using the HS256 signing method
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token using the secret key
+	tokenString, err := token.SignedString(jwtSecretKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+
+	return tokenString, nil
+}
+
+func VerifyJWT(tokenString string) (*CustomClaims, error) {
+	// Parse the token
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// Make sure the signing method is HMAC (HS256)
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtSecretKey, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token: %w", err)
+	}
+
+	// Extract claims if the token is valid
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
 }
